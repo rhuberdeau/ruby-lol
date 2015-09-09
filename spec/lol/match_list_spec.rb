dfdeed948cba54522e46373d10c447bdbf507189
rhuberdeau <rhuberdeau@gmail.com>
2015-09-09 10:30:56 -0400

    Reworking match_list test to use VCR

--- spec/lol/match_list_spec.rb
+++ spec/lol/match_list_spec.rb
@@ -8,20 +8,22 @@ describe MatchList do
     expect(MatchList.ancestors[1]).to eq(Request)
   end
 
-  let(:request) { MatchList.new("api_key", "euw") }
+  let(:request) { MatchList.new(ENV.fetch("RIOT_API_KEY"), "na") }
 
   describe "#get" do
-    subject { request.get(1) }
-
-    before { stub_request(request, 'match_list', 'matchlist/1') }
-
-    it 'returns an hash' do
-      expect(subject).to be_a(Hash)
+    it 'returns an array of hashes' do
+      VCR.use_cassette("matchlist") do 
+        matches = request.get(20833616, {rankedQueues: "RANKED_SOLO_5x5", season: "SEASON2015"})
+        expect(matches.first.last.class).to eql(Array)
+        expect(matches.first.last.first.class).to eql(Hash)
+      end
     end
 
     it 'fetches matches from the API' do
-      fixture = load_fixture('match_list', MatchList.api_version)
-      expect(subject.keys).to match_array fixture.keys
+      VCR.use_cassette("matchlist") do 
+        matches = request.get(20833616, {rankedQueues: "RANKED_SOLO_5x5", season: "SEASON2015"})
+        expect(matches.first.last.length).to be >= 471
+      end
     end
   end
 
